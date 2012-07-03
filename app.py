4f9f0e0084ba235425b5cb6ba3dc99504cc3da07
Romack Natividad <romack@wizmediateam.com>
2012-07-04 00:49:19 +0800

    added github image that bounces on screen

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 # @author Romack Natividad <romacknatividad@gmail.com>
 
 import pygame
+import random
 from pygame.locals import *
 
 def main():
@@ -20,10 +21,12 @@ def main():
 	orig_mario = pygame.image.load("images/Mario.gif")
 	supermario = pygame.image.load("images/Super Mario.gif")
 	fierymario = pygame.image.load("images/Fiery Mario.gif")
+	github = pygame.image.load("images/github.jpg")
 	mario = orig_mario
 	icon_status = "mario"
 
-	x,y,xs,ys,step_x,step_y = 0,182,screen_width,0,2,2
+	x,y,step_x,step_y = random.randint(0,screen_width-1),random.randint(0,screen_height-1),2,2
+	gx,gy,step_gx,step_gy = random.randint(0,screen_width-1),random.randint(0,screen_height-1),2,2
 
 	clock = pygame.time.Clock()
 
@@ -34,21 +37,21 @@ def main():
 			step_x = -step_x
 		if y > screen_height - 32 or y < 0:
 			step_y = -step_y
+		if gx > screen_width - 16 or gx < 0:
+                        step_gx = -step_gx
+                if gy > screen_height - 16 or gy < 0:
+                        step_gy = -step_gy
+
 		x += step_x
 		y += step_y
-
-		screen.fill((255,255,255)) # white BG
-		screen.blit(bg, (0,0))
-		screen.blit(morefun, (screen_width-407, screen_height-150))
-		pygame.draw.line(screen, (0,0,0),(0,0),(screen_width, screen_height))
-		screen.blit(mario, (x, y))
-		pygame.display.flip()
+		gx += step_gx
+		gy += step_gy
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
-			elif event.type == pygame.MOUSEMOTION:
-				print "Mouse @ (%d %d)" % event.pos
+			#elif event.type == pygame.MOUSEMOTION:
+			#	print "Mouse @ (%d %d)" % event.pos
 			elif event.type == pygame.MOUSEBUTTONDOWN:
 				pos = event.pos
 				click_x = pos[0]
@@ -64,6 +67,14 @@ def main():
 					else:
 						mario = orig_mario
 						icon_status = "mario"
+
+		screen.fill((255,255,255)) # white BG
+                screen.blit(bg, (0,0))
+                screen.blit(morefun, (screen_width-407, screen_height-150))
+                pygame.draw.line(screen, (0,0,0),(0,0),(screen_width, screen_height))
+                screen.blit(mario, (x, y))
+                screen.blit(github,(gx,gy))
+                pygame.display.flip()
 
 		clock.tick(100)
 						
